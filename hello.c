dfc9823e5867dc39fda190b92ac45057c5099357
sun2011yao <sun2011yao@163.com>
2016-09-22 06:55:01 +0800

    hello

--- hello.c
+++ hello.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    printf("helldggo world, I am very happniess!!!\n");
+    printf("hello world, I am very happniess!!!\n");
     printf("hello world, I am very happniess!!!\n");
     printf("I want to be a programming expert!!!!");
     return 0;
